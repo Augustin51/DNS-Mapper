282feca3cbe53e41c747dbb43495e8115b16016f
Augutin51 <jacquesson.augustin@gmail.com>
2026-01-08 12:31:59 +0100

    add A and AAAA records

--- dns_mapper.py
+++ dns_mapper.py
@@ -1,14 +1,37 @@
 import argparse
+import dns.resolver
 
-def parserArg() :
+def parserArg():
     parser = argparse.ArgumentParser()
     parser.add_argument("domainName")
     args = parser.parse_args()
     return args
 
-def main() :
-    args = parserArg()
-    return args.domainName
+def resolve_ip(domain):
+    result = {}
 
-if __name__ == "__main__" :
-    print(main())
+    try:
+        A_records = dns.resolver.resolve(domain, 'A')
+        result['A'] = [rdata.to_text() for rdata in A_records]
+    except:
+        result['A'] = ["No A record found"]
+
+    try:
+        AAAA_records = dns.resolver.resolve(domain, 'AAAA')
+        result['AAAA'] = [rdata.to_text() for rdata in AAAA_records]
+    except:
+        result['AAAA'] = ["No AAAA record found"]
+
+    return result
+
+def show_result(result):
+    for res in result:
+        print(f"{res} records : {result[res]}")
+
+def main():
+    args = parserArg()
+    result = resolve_ip(args.domainName)
+    show_result(result)
+
+if __name__ == "__main__":
+    main()
