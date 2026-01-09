4f815ea8d7a0812e6fd0a50f27244ef5ea31353e
Augustin <150128467+Augustin51@users.noreply.github.com>
2026-01-09 09:36:18 +0100

    Revert "add A and AAAA records"

--- dns_mapper.py
+++ dns_mapper.py
@@ -1,37 +1,14 @@
 import argparse
-import dns.resolver
 
-def parserArg():
+def parserArg() :
     parser = argparse.ArgumentParser()
     parser.add_argument("domainName")
     args = parser.parse_args()
     return args
 
-def resolve_ip(domain):
-    result = {}
-
-    try:
-        A_records = dns.resolver.resolve(domain, 'A')
-        result['A'] = [rdata.to_text() for rdata in A_records]
-    except:
-        result['A'] = ["No A record found"]
-
-    try:
-        AAAA_records = dns.resolver.resolve(domain, 'AAAA')
-        result['AAAA'] = [rdata.to_text() for rdata in AAAA_records]
-    except:
-        result['AAAA'] = ["No AAAA record found"]
-
-    return result
-
-def show_result(result):
-    for res in result:
-        print(f"{res} records : {result[res]}")
-
-def main():
+def main() :
     args = parserArg()
-    result = resolve_ip(args.domainName)
-    show_result(result)
+    return args.domainName
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__" :
+    print(main())
