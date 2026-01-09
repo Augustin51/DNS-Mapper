d3f3e89c4c35c17ea4aba9bab83c143958b579be
Augutin51 <jacquesson.augustin@gmail.com>
2026-01-09 12:17:09 +0100

    Extract ip from TXT records

--- dns_mapper.py
+++ dns_mapper.py
@@ -24,17 +24,23 @@ def resolve_records(domain):
 
 def parse_txt_records(txt_records) : 
     new_domains = []
+    new_IPs_address = []
     for txt_record in txt_records :
-        if not txt_record.startswith('"v=spf1'):
-            continue
-
-        new_domain = find_new_domain(txt_record)
+        # DOMAIN
+        new_domain = extract_new_domain(txt_record)
         if new_domain :
             for i in new_domain : 
                 new_domains.append(i)
-    return new_domains
 
-def find_new_domain(txt_record):
+        # IP ADDRESS
+        new_IP_address = extract_new_ip(txt_record)
+        if new_IP_address :
+            for i in new_IP_address :
+                new_IPs_address.append(i)
+        
+    return new_domains, new_IPs_address
+
+def extract_new_domain(txt_record):
     return re.findall(
         rf"(?:[a-z0-9_]" + 
         rf"(?:[a-z0-9-_]{{0,61}}" + 
@@ -45,6 +51,11 @@ def find_new_domain(txt_record):
         flags=re.IGNORECASE,
     )
 
+def extract_new_ip(txt_record) :
+    return re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',
+        txt_record,
+        flags=re.IGNORECASE,)
+
 
 def show_result(result):
     for res in result:
@@ -52,13 +63,18 @@ def show_result(result):
 
 def main():
     new_domains = []
+    new_IPs_address = []
     args = parserArg()
     result = resolve_records(args.domainName)
-    new_domains_temp = parse_txt_records(result['TXT'])
+    new_domains_temp, new_IPs_address_temp = parse_txt_records(result['TXT'])
     for i in new_domains_temp : 
         new_domains.append(i)
+    for i in new_IPs_address_temp : 
+        new_IPs_address.append(i)
+
     show_result(result)
     print(f"\n Domain à réutilisé : \n {new_domains}")
+    print(f"\n IPs à réutilisé : \n {new_IPs_address}")
 
 
 if __name__ == "__main__":
